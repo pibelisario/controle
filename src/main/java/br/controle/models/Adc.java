717af811497e9afc3e50e90e552e64aef17a3004
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-02-20 16:57:51 -0300

    CRIAÇÃO MODEL ADC

---
+++ src/main/java/br/controle/models/Adc.java
@@ -0,0 +1,115 @@
+package br.controle.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+public class Adc {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @NotEmpty
+    private String rg;
+    @NotNull
+    @NotEmpty
+    private String nome;
+    private String cpf;
+    @NotNull
+    @NotEmpty
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate dia;
+    @NotNull
+    @NotEmpty
+    private Date hora;
+    @NotNull
+    @NotEmpty
+    private String local;
+    @NotNull
+    @NotEmpty
+    private String tipo;
+
+    public Adc() {
+    }
+
+    public Adc(Long id, String rg, String nome, String cpf, LocalDate dia, Date hora, String local, String tipo) {
+        this.id = id;
+        this.rg = rg;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dia = dia;
+        this.hora = hora;
+        this.local = local;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDia() {
+        return dia;
+    }
+
+    public void setDia(LocalDate dia) {
+        this.dia = dia;
+    }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
