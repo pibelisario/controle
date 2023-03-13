0359f0e8d8bb0ffd9275eaeb53e653411bbb3d74
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-03-13 10:49:05 -0300

    limpando imports

--- src/main/java/br/controle/repositories/EntradaRepository.java
+++ src/main/java/br/controle/repositories/EntradaRepository.java
@@ -1,16 +1,12 @@
 package br.controle.repositories;
 
-import br.controle.models.Cadastro;
 import br.controle.models.Entrada;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.w3c.dom.ElementTraversal;
 
 import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface EntradaRepository extends JpaRepository<Entrada, Long> {
--- src/main/java/br/controle/services/EntradaService.java
+++ src/main/java/br/controle/services/EntradaService.java
@@ -1,16 +1,14 @@
 package br.controle.services;
 
-import br.controle.models.Cadastro;
 import br.controle.models.Entrada;
 import br.controle.repositories.EntradaRepository;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 
 @Service
 public class EntradaService {
@@ -32,16 +30,6 @@ public class EntradaService {
         entrada.setData(new Date());
         entradaRepository.save(entrada);
     }
-
-//    public List<Entrada> buscarPorDatas(String dataInicial, String dataFinal) throws ParseException {
-//        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-//        Date dataInicialF = formato.parse(dataInicial);
-//        Date dataFinalF = formato.parse(dataFinal);
-//        dataFinalF.setHours(23);
-//        List<Entrada> entradas = entradaRepository.findEntradasByDataBetween(dataInicialF, dataFinalF);
-//        Collections.reverse(entradas);
-//        return entradas;
-//    }
 
     public Page<Entrada> buscarPorDatas(String dataInicial, String dataFinal, Pageable pageable) throws ParseException {
         SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
