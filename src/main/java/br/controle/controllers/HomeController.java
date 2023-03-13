01c1def358e7313a73c99a32c4c6e199a7288523
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-03-13 09:32:45 -0300

    ajustando quantidade de entradas pag

---
+++ src/main/java/br/controle/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package br.controle.controllers;
+
+import br.controle.services.EntradaService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    EntradaService entradaService;
+
+    public HomeController(EntradaService entradaService) {
+        this.entradaService = entradaService;
+    }
+
+    @GetMapping("/")
+    public ModelAndView entradas(){
+        ModelAndView mv = new ModelAndView("/entradas");
+        mv.addObject("entradas", entradaService.findAll(PageRequest.of(0,5, Sort.by("id").descending())));
+        return mv;
+    }
+}
