ee6857c9b0d90c8703d31c116a111d3241e4aeed
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-03-15 16:52:20 -0300

    Alterando metodo atualizar cadastro part I

--- src/main/java/br/controle/controllers/CadastroController.java
+++ src/main/java/br/controle/controllers/CadastroController.java
@@ -115,7 +115,7 @@ public class CadastroController {
     }
 
     @GetMapping("/editarCadastro/{idCadastro}")
-    public ModelAndView editarCadasro(@PathVariable("idCadastro") Long id) {
+    public ModelAndView editarCadastro(@PathVariable("idCadastro") Long id) {
         ModelAndView mv = new ModelAndView("/editar");
         mv.addObject("objCadastro", cadastroService.findById(id));
         mv.addObject("listaCategorias", CategoriaCadastro.values());
@@ -123,7 +123,7 @@ public class CadastroController {
         return mv;
     }
 
-    @PostMapping("/editarCadastro/{idCadastro}")
+   @PostMapping("/editarCadastro/{idCadastro}")
     public ModelAndView editarCadastro(@PathVariable("idCadastro") Long id, @Valid Cadastro cadastro, BindingResult result
             , RedirectAttributes attributes){
         ModelAndView mv = new ModelAndView();
